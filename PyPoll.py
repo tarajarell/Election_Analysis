4d8485023c12414057b95f05973b98c51274bdef
tarajarellgit <ms.taraj@gmail.com>
2020-08-01 22:16:40 -0700

    Adding newest version of module walkthrough redo.

--- PyPoll.py
+++ PyPoll.py
@@ -1,32 +1,25 @@
-
-# Write down the names of all the candidates.
-# Add a vote count for each candidate.
-# Get the total votes for each candidate.
-# Get the total votes cast for the election.
-
-# Open the data file/add our dependencies.
+# The data we need to retrieve.
 import csv
 import os
 
-# Assign a variable for the file to load from a path (3.4.2)
-file_to_load = os.path.join("resources", "election_results.csv")
-# Assign a variable to save the file to a path (3.4.2)
-flie_to_save = os.path.join("analysis", "election_analysis.txt")
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# open the election results and read the file (3.4.2)
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    #Read the CSV
-    csvread = csv.reader(election_data, delimiter=',')
-
-    #Read the first row as header
-    csv.header = next(csvread)
-
-    #Establish values for all candidates at once
-    candidate_options = []
-    candidate_votes = {}
+    # To do: read and analyse the data here.
+    file_reader = csv.reader(election_data)
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
 
 
-#Using the opan()function with the "w" mode we will write data to the file
-with open(file_to_save, "w") as txt_file
-    
+
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
