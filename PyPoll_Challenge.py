e79297497033e1bf2f8f153f31fc17e788fcc2ee
tarajarellgit <ms.taraj@gmail.com>
2020-08-02 16:48:17 -0700

    Deliverable 2 done as well

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -102,7 +102,7 @@ with open(file_to_save, "w") as txt_file:
          # 6d: Print the county results to the terminal.
         print(county_results)
          # 6e: Save the county votes to a text file.
-
+        txt_file.write(county_results)
          # 6f: Write a decision statement to determine the winning county and get its vote count.
         if (votes > county_turnout):
             county_turnout = votes
@@ -116,7 +116,7 @@ with open(file_to_save, "w") as txt_file:
     print(winning_county_summary)
 
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(winning_county_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
